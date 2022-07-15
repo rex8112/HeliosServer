f479a093a2e5479bb02cbb956a351fa62c8a8caa
Riley Winkler <rex8112@gmail.com>
2022-07-14 19:41:56 -0500

    Add settings and flags to Channel

--- api/serializers.py
+++ api/serializers.py
@@ -1,11 +1,12 @@
-from .models import Server, Channel
 from rest_framework import serializers
+
+from .models import Server, Channel
 
 
 class ChannelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Channel
-        fields = ['id', 'server', 'type']
+        fields = ['id', 'server', 'type', 'settings', 'flags']
 
 
 class ServerSerializer(serializers.ModelSerializer):
