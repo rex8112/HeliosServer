e8ab7017d16c30201b5e480871efe26de290a911
Riley Winkler <rex8112@gmail.com>
2022-08-09 19:17:12 -0500

    Convert to MySql

--- heliosserver/settings.py
+++ heliosserver/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'heliosserver.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': str(BASE_DIR / 'my.cnf')
+        },
     }
 }
 
